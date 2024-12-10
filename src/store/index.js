85b585196e51536239de4916f21af72d107b4df0
Elena Orekhova <lelyaorol@gmail.com>
2024-12-10 15:16:44 +0300

    fix get list by button

--- src/store/index.js
+++ src/store/index.js
@@ -2,4 +2,7 @@ import { applyMiddleware, createStore } from "redux";
 import { customerReducer } from "./customerReduser";
 import { thunk } from "redux-thunk";
 
-export const store = createStore(customerReducer)
+export const store = createStore(
+  customerReducer,
+  applyMiddleware(thunk)
+);
